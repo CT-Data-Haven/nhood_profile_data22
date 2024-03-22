0ef8f165d4c7de9e12fb299ea1cb46018823b71c
camille-s <camille.seaberry@gmail.com>
2024-03-22 18:26:36 -0400

    Viz data should have keys in snakecase

--- scripts/03_prep_json_to_viz.R
+++ scripts/03_prep_json_to_viz.R
@@ -12,12 +12,13 @@ acs_year <- snakemake@params[["acs_year"]]
 #         "level": "1_state",
 #         "location": "Connecticut"
 # ...
-prof_wide <- readRDS(file.path("output_data", stringr::str_glue("all_nhood_{acs_year}_acs_health_comb.rds"))) %>%
-  map(mutate, across(c(topic, indicator), forcats::as_factor)) %>%
-  map(select, -year) %>%
-  map(rename, location = name) %>%
-  map(~ split(., .$topic)) %>%
-  map_depth(2, tidyr::pivot_wider, names_from = indicator) %>%
+prof_wide <- readRDS(file.path("output_data", stringr::str_glue("all_nhood_{acs_year}_acs_health_comb.rds"))) |>
+  set_names(snakecase::to_snake_case) |>
+  map(mutate, across(c(topic, indicator), forcats::as_factor)) |>
+  map(select, -year) |>
+  map(rename, location = name) |>
+  map(~ split(., .$topic)) |>
+  map_depth(2, tidyr::pivot_wider, names_from = indicator) |>
   map_depth(2, select, -topic)
 
 jsonlite::write_json(prof_wide, file.path("to_viz", stringr::str_glue("nhood_wide_{acs_year}.json")), auto_unbox = TRUE)
