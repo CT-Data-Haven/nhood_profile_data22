9ab94a8dd8c86336ff5db6676f56ef8d72ba4921
camille-s <camille.seaberry@gmail.com>
2024-04-23 12:59:30 -0400

    Fix gaps in shapefiles

--- scripts/04_make_shapefiles.R
+++ scripts/04_make_shapefiles.R
@@ -5,11 +5,11 @@ sf::sf_use_s2(FALSE)
 
 ############ SHAPEFILES ################################################
 # would love a json file of readable topojson, but last time that didn't seem possible
-shps <- tibble::lst(cwi::bridgeport_sf, cwi::hartford_sf, cwi::new_haven_sf, cwi::stamford_sf) %>%
-  set_names(stringr::str_extract, "([a-z_]+)(?=_sf)") %>%
-  map(st_transform, 4326) %>%
-  map(st_cast, "MULTIPOLYGON") %>%
-  imap(function(shp, city) mutate(shp, name = ifelse(name %in% dupe_nhoods, paste(name, stringr::str_to_title(city)), as.character(name)))) %>%
+shps <- tibble::lst(cwi::bridgeport_sf, cwi::hartford_sf, cwi::new_haven_sf, cwi::stamford_sf) |>
+  set_names(stringr::str_extract, "([a-z_]+)(?=_sf)") |>
+  map(st_transform, 4326) |>
+  map(st_cast, "MULTIPOLYGON") |>
+  imap(function(shp, city) mutate(shp, name = ifelse(name %in% dupe_nhoods, paste(name, stringr::str_to_title(city)), as.character(name)))) |>
   map(function(shp) mutate(shp, name = stringr::str_replace(name, "\\bOf\\b", "of") %>% stringr::str_replace("New_haven", "New Haven")))
 
 # topojson_write is deprecated
@@ -17,9 +17,14 @@ shps <- tibble::lst(cwi::bridgeport_sf, cwi::hartford_sf, cwi::new_haven_sf, cwi
 # geojsonio::topojson_write(shp, object_name = "city", file = file.path("to_viz", "cities", paste(city, "topo.json", sep = "_")))
 # })
 
-shps %>%
-  imap(function(shp, city) geojsonio::geojson_write(shp, object_name = "city", file = file.path("to_viz", "cities", paste(city, "topo.json", sep = "_")))) %>%
-  map(pluck, "path") %>%
+shps |>
+  imap(function(shp, city) {
+    geojsonio::geojson_write(shp,
+      object_name = "city",
+      file = file.path("to_viz", "cities", paste(city, "topo.json", sep = "_"))
+    )
+  }) |>
+  map(pluck, "path") |>
   walk(function(pth) {
-    system(stringr::str_glue("mapshaper {pth} -o force format=topojson {pth}"))
+    system(stringr::str_glue("mapshaper {pth} -clean -filter-slivers -o force format=topojson {pth}"))
   })
